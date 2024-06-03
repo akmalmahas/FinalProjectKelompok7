a150ac97683083aefabf4b143ddfbbae49d267ab
akmalmahas <dracarys.co@gmail.com>
2024-06-03 11:17:32 +0700

    penambahan tabel

--- FinalProjectKelompok7/Initial_db.sql
+++ FinalProjectKelompok7/Initial_db.sql
@@ -26,10 +26,39 @@ CREATE TABLE tbl_departments (
 	locations int not null
 );
 
+--JOBS
+CREATE TABLE tbl_jobs(
+	id VARCHAR(10) PRIMARY KEY,
+	title VARCHAR(35) NOT NULL,
+	min_salary INT(11),
+	max_salary INT(11)
+);
+
+--JOBS HISTORIES
+CREATE TABLE tbl_job_histories(
+	employee INT(6) IDENTITY PRIMARY KEY,
+	start_date date PRIMARY KEY,
+	end_date date,
+	status VARCHAR(10) NOT NULL,
+	job VARCHAR(10) NOT NULL,
+	department INT(4) NOT NULL
+);
+
+--ACCOUNTS
+CREATE TABLE tbl_accounts(
+	id INT IDENTITY PRIMARY KEY,
+	username VARCHAR(25),
+	password VARCHAR(255) NOT NULL,
+	otp INT,
+	is_expired DATETIME,
+	is_used BIT
+);
+
 
 
 --Pembuatan Foreign Key
-
 ALTER TABLE tbl_countries ADD FOREIGN KEY(region) REFERENCES tbl_regions(id);
 ALTER TABLE tbl_locations ADD FOREIGN KEY(country) REFERENCES tbl_countries(id);
 ALTER TABLE tbl_departments ADD FOREIGN KEY(location) REFERENCES tbl_locations(id);
+ALTER TABLE tbl_job_histories ADD FOREIGN KEY(employee) REFERENCES tbl_employees(id);
+ALTER TABLE tbl_accounts ADD FOREIGN KEY(id) REFERENCES tbl_employees(id);

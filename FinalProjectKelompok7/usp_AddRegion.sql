837c7be7647be34b3d8cc57df37f681d41337338
zidana10-a <119581787+zidana10-a@users.noreply.github.com>
2024-06-04 10:51:29 +0700

    pembuatan usp_AddRegion

---
+++ FinalProjectKelompok7/usp_AddRegion.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE AddRegion
+    @name VARCHAR(25)
+AS
+BEGIN
+    -- Menambahkan data ke tabel Region
+    INSERT INTO tbl_regions (name)
+    VALUES (@name);
+    
+    -- Mengembalikan id dari record yang baru ditambahkan
+    SELECT SCOPE_IDENTITY() AS NewID;
+END;
+
+/*
+INSERT INTO tbl_regions (name)
+VALUES ('ASIA');
+
+DELETE FROM tbl_regions
+WHERE name = 'Africa';
+
+Select * from tbl_regions;
+*/
+
+DECLARE @NewID INT;
+EXEC AddRegion @name = 'Africa';
+SELECT @NewID = SCOPE_IDENTITY();
+SELECT @NewID AS IDBaru;
+
+DBCC CHECKIDENT ('tbl_regions', RESEED, 1);

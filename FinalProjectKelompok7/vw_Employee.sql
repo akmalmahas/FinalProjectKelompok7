dba3f081726e2c2f75cd2d16385e0cb898d9d232
rglramadhan <ragilramadhan1022@gmail.com>
2024-06-06 13:04:10 +0700

    view employee

---
+++ FinalProjectKelompok7/vw_Employee.sql
@@ -0,0 +1,11 @@
+CREATE VIEW vw_Employee AS
+SELECT 
+    e.id,
+    CONCAT(e.first_name, ' ', e.last_name) AS fullname,
+    j.id AS job,
+    d.id AS department
+    
+FROM tbl_employees e
+INNER JOIN tbl_jobs j ON j.id = e.job
+INNER JOIN tbl_departments d ON d.id = e.department;
+

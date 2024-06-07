a0d0e760abab34421c031f449976106cb6959678
akmalmahas <dracarys.co@gmail.com>
2024-06-07 17:03:24 +0700

    edit vw

---
+++ FinalProjectKelompok7/vw_EmployeeDetail.sql
@@ -0,0 +1,15 @@
+CREATE VIEW vw_EmployeeDetails AS
+SELECT 
+    id,
+    CONCAT(first_name, ' ', last_name) AS fullname,
+    gender,
+    email, 
+	phone, 
+	hire_date, 
+	salary, 
+	manager, 
+	job, 
+	department
+FROM tbl_employees
+
+SELECT * FROM vw_EmployeeDetail;
---
+++ FinalProjectKelompok7/vw_Profile.sql
@@ -0,0 +1,13 @@
+CREATE VIEW vw_Profile AS
+SELECT 
+    e.id,
+	a.username,
+    CONCAT(e.first_name, ' ', e.last_name) AS fullname,
+    e.gender,
+	e.email,
+	e.phone
+    
+FROM tbl_employees e
+INNER JOIN tbl_accounts a ON a.id = e.id
+
+SELECT * FROM vw_Profile;
